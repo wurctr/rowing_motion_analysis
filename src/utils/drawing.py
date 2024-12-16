a3321a2c41397e624d51781f35592420232ce7fc
wurctr <wurc.trainer@gmail.com>
2024-12-16 18:42:36 +0900

    Update drawing.py

--- src/utils/drawing.py
+++ src/utils/drawing.py
@@ -1,16 +1,40 @@
 import cv2
 
-def draw_landmarks_with_skeleton(frame, landmarks):
-    """ランドマークと骨格を描画"""
-    for lm in landmarks:
-        x, y = int(lm.x * frame.shape[1]), int(lm.y * frame.shape[0])
-        cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
 
-    # 骨格の例: 肩 - 肘 - 手首
-    connections = [(11, 13), (13, 15)]  # Mediapipeのランドマーク番号
-    for start, end in connections:
-        start_point = (int(landmarks[start].x * frame.shape[1]), int(landmarks[start].y * frame.shape[0]))
-        end_point = (int(landmarks[end].x * frame.shape[1]), int(landmarks[end].y * frame.shape[0]))
-        cv2.line(frame, start_point, end_point, (255, 0, 0), 2)
+def draw_pelvis_bounds(image, pelvis_bounds, color=(0, 255, 255)):
+    """
+    骨盤を囲む四角形を描画。
+    - image: 描画対象の画像（numpy array）。
+    - pelvis_bounds: 骨盤を囲む四角形の頂点リスト [(x1, y1), (x2, y2), ...]。
+    - color: 四角形の色 (B, G, R)。
+    """
+    top_left, top_right, bottom_left, bottom_right = pelvis_bounds
 
-    return frame
+    # 四角形を描画
+    cv2.line(image, (int(top_left[0]), int(top_left[1])), (int(top_right[0]), int(top_right[1])), color, 2)
+    cv2.line(image, (int(top_right[0]), int(top_right[1])), (int(bottom_right[0]), int(bottom_right[1])), color, 2)
+    cv2.line(image, (int(bottom_right[0]), int(bottom_right[1])), (int(bottom_left[0]), int(bottom_left[1])), color, 2)
+    cv2.line(image, (int(bottom_left[0]), int(bottom_left[1])), (int(top_left[0]), int(top_left[1])), color, 2)
+
+    return image
+
+
+def draw_landmarks(image, landmarks, connections=None, color=(0, 255, 0)):
+    """
+    ランドマークを画像上に描画。
+    - image: 描画対象の画像。
+    - landmarks: ランドマークのリスト [(x1, y1), (x2, y2), ...]。
+    - connections: ランドマーク間の接続リスト [(start_idx, end_idx), ...]。
+    - color: 描画する色。
+    """
+    for idx, lm in enumerate(landmarks):
+        x, y = int(lm[0]), int(lm[1])
+        cv2.circle(image, (x, y), 5, color, -1)
+
+    if connections:
+        for start_idx, end_idx in connections:
+            start_point = (int(landmarks[start_idx][0]), int(landmarks[start_idx][1]))
+            end_point = (int(landmarks[end_idx][0]), int(landmarks[end_idx][1]))
+            cv2.line(image, start_point, end_point, color, 2)
+
+    return image
